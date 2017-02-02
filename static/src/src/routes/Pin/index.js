579c7f314b3be1e84d8dd3b47101850960135395
Darren Frenkel <darrenfrenkel@gmail.com>
2017-02-02 21:40:28 +1100

    fix spelling and spacing errors

--- static/src/src/routes/Pin/index.js
+++ static/src/src/routes/Pin/index.js
@@ -4,10 +4,10 @@ export default (store) => ({
   path: 'pin',
   getComponent (nextState, next) {
     require.ensure([
-      './containers/pinContainer',
+      './containers/PinContainer',
       './modules/pin'
     ], (require) => {
-      const Pin = require('./containers/pinContainer').default;
+      const Pin = require('./containers/PinContainer').default;
       const pinReducer = require('./modules/pin').default;
 
       injectReducer(store, {
