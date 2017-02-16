038d2756222f174f1e23ed38797845565fcaa5a7
Darren Frenkel <darrenfrenkel@gmail.com>
2017-02-16 22:35:24 +1100

    address-changes - code linter cleanup

--- apps/pindrop/tests/test_models.py
+++ apps/pindrop/tests/test_models.py
@@ -6,6 +6,7 @@ from django.utils import timezone
 from apps.pindrop.factories import PinFactory
 import collections
 
+
 class PinDropTestCase(TestCase):
 
     def test_updating_status_to_middle(self):
@@ -29,24 +30,29 @@ class PinDropTestCase(TestCase):
         pindrop.update_status()
         self.assertEqual(pindrop.status, pindrop.STATE.REMOVED)
 
-class Add_GeocoordinatesTestCast(TestCase):
+
+class AddGeocoordinatesTestCast(TestCase):
     def test_returning_latitude_and_longditude(self):
       
-        address = collections.namedtuple('address', ['address_street','address_city', 'address_postal_code', 'googleLat', 'googleLon'] )
-        addressBook = [address('2 Dunbar Ave','Melbourne', 3161, -37.878505, 145.019607),
-                       address('19 Dunbar Ave','Melbourne', 3161, -37.878945, 145.021097),
-                       address('15 Stkilda Rd', 'Melbourne', 3004, -37.818164, 144.967869) ]
+        address = collections.namedtuple(
+            'address',
+            ['address_street', 'address_city', 'address_postal_code', 'googleLat', 'googleLon']
+        )
+        address_book = [
+            address('2 Dunbar Ave', 'Melbourne', 3161, -37.878505, 145.019607),
+            address('19 Dunbar Ave', 'Melbourne', 3161, -37.878945, 145.021097),
+            address('15 St kilda Rd', 'Melbourne', 3004, -37.818164, 144.967869)
+        ]
         
-        #Set decimal accuracy of the GPS coordinates
+        # Set decimal accuracy of the GPS coordinates
         accuracy = 2 
-        for i in addressBook:
-            pindrop = PinFactory(address_street=i.address_street, address_city=i.address_city, address_postal_code=i.address_postal_code)
-            pindrop.add_geocoordinates() 
-            rndPinLatLon = (round(pindrop.latitude, accuracy) , round(pindrop.longitude, accuracy))
-            rndGoogleLatLon = (round(i.googleLat, accuracy), round(i.googleLon, accuracy))
-            self.assertEqual(rndPinLatLon, rndGoogleLatLon)
-        
-
-
-
-
+        for i in address_book:
+            pindrop = PinFactory(
+                address_street=i.address_street,
+                address_city=i.address_city,
+                address_postal_code=i.address_postal_code
+            )
+            pindrop.add_geocoordinates()
+            round_pin = (round(pindrop.latitude, accuracy), round(pindrop.longitude, accuracy))
+            round_google = (round(i.googleLat, accuracy), round(i.googleLon, accuracy))
+            self.assertEqual(round_pin, round_google)
