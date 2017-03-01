ca9058a83c18495616d37bb2f58721b184337945
Darren Frenkel <darrenfrenkel@gmail.com>
2017-03-01 23:03:47 +1100

    KUR-17 - close pin panel

--- static/src/src/routes/Pin/components/Panel.js
+++ static/src/src/routes/Pin/components/Panel.js
@@ -1,20 +1,33 @@
 import React from 'react';
-import classNames from 'classnames';
 
 export default function Panel (props) {
+  const {
+    activePin,
+    closePin
+  } = props
   return (
     <aside className='side-panel'>
-      <p>{props.props.address_number} {props.props.address_street}</p>
-      <p>{props.props.address_city}, {props.props.address_postal_code}</p>
-      {props.props.items.map((item, index) => {
+      <p>{activePin.address_number} {activePin.address_street}</p>
+      <p>{activePin.address_city}, {activePin.address_postal_code}</p>
+      {activePin.items.map((item, index) => {
         return (
           <li key={index}>{item.name}</li>
         );
       })}
-
+      <button
+        onClick={
+          function () {
+            closePin();
+          }}
+      >Close</button>
     </aside>
   );
 }
 
 Panel.propTypes = {
+};
+
+Panel.propTypes = {
+  activePin: React.PropTypes.bool,
+  closePin: React.PropTypes.func
 };
